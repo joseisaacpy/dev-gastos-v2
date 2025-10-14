7fb8f424f09a08fbd8bd3807b9b4e91ca1cc0a2c
José Isaac <joseisaacnascimento@gmail.com>
2025-10-14 14:45:11 -0300

    style: adicionar cor de fundo na página de erro/not-found - adicionar icon no botão de voltar para "/"

--- src/app/not-found.jsx
+++ src/app/not-found.jsx
@@ -1,8 +1,30 @@
+// Components
+import { Button } from "@/components/ui/button";
+
+// Link
+import Link from "next/link";
+// Icon
+import { ArrowLeft } from "lucide-react";
+
 export default function NotFound() {
   return (
-    <div>
-      <h1>Pagina Nao Encontrada</h1>
-      <p>Ocorreu um erro ao tentar acessar essa pagina.</p>
-    </div>
+    <section className="w-full h-screen px-3 md:p-0 flex flex-col items-center justify-center gap-2 text-center bg-gradient-to-l from-gray-600 to-gray-800 text-white">
+      <h1 className="text-2xl md:text-3xl font-bold">
+        Ops, página não encontrada!
+      </h1>
+      <p className="text-lg md:text-xl">
+        Ocorreu um erro ao tentar acessar essa pagina.
+      </p>
+      <Link href="/">
+        <Button
+          size={"lg"}
+          className={
+            "cursor-pointer transition-all duration-300 hover:scale-110"
+          }
+        >
+          Voltar para o início <ArrowLeft className="ml-2 animate-pulse" />
+        </Button>
+      </Link>
+    </section>
   );
 }
