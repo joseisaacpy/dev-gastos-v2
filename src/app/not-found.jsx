59af525fc3b2f21f3f527cf393ccaa02ad3efe6f
José Isaac <joseisaacnascimento@gmail.com>
2025-10-14 12:11:09 -0300

    chore: preparar organização de pastas e dependências do projeto

---
+++ src/app/not-found.jsx
@@ -0,0 +1,8 @@
+export default function NotFound() {
+  return (
+    <div>
+      <h1>Pagina Nao Encontrada</h1>
+      <p>Ocorreu um erro ao tentar acessar essa pagina.</p>
+    </div>
+  );
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,10 @@
+export default function Home() {
+  return (
+    <h1>
+      Lorem, ipsum dolor sit amet consectetur adipisicing elit. Magnam fugit
+      voluptates officiis asperiores nesciunt quidem unde cupiditate id enim,
+      laboriosam, aliquam odit possimus tempora incidunt placeat. Provident quae
+      omnis ipsa!
+    </h1>
+  );
+}
