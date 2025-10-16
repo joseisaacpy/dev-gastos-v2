cedb2c9fdd18065e4b14d6ff12ee051984be686e
José Isaac <joseisaacnascimento@gmail.com>
2025-10-15 22:03:04 -0300

    refactor: organizar links do sistema no array de links separado

--- src/utils/itemsNav.js
+++ src/utils/itemsNav.js
@@ -1,42 +1,42 @@
 // Icons
-import { DollarSign, BarChart3, Settings, PlusCircle } from "lucide-react";
+import { Home, DollarSign, BarChart3, PlusCircle } from "lucide-react";
 
 const itemsNav = [
+  {
+    name: "Início",
+    description: "Acesse o painel de controle.",
+    href: "/",
+    icon: <Home className="w-6 h-6" />,
+  },
   {
     name: "Gastos",
     description: "Veja e gerencie todos os seus gastos registrados.",
     href: "/gastos",
-    icon: <DollarSign className="w-5 h-5" />,
+    icon: <DollarSign className="w-6 h-6" />,
   },
   {
     name: "Novo Gasto",
     description: "Adicione uma nova despesa ao seu controle.",
     href: "/novo-gasto",
-    icon: <PlusCircle className="w-5 h-5" />,
+    icon: <PlusCircle className="w-6 h-6" />,
   },
   {
     name: "Nova Receita",
     description: "Registre um novo ganho ou entrada de dinheiro.",
     href: "/nova-receita",
-    icon: <PlusCircle className="w-5 h-5" />,
+    icon: <PlusCircle className="w-6 h-6" />,
   },
   {
     name: "Receitas",
     description: "Acompanhe todas as suas entradas de renda.",
     href: "/receitas",
-    icon: <DollarSign className="w-5 h-5" />,
+    icon: <DollarSign className="w-6 h-6" />,
   },
   {
     name: "Resumo",
     description: "Visualize um balanço geral das suas finanças.",
-    href: "/resumo",
-    icon: <BarChart3 className="w-5 h-5" />,
-  },
-  {
-    name: "Configurações",
-    description: "Ajuste preferências da conta e do sistema.",
-    href: "/config",
-    icon: <Settings className="w-5 h-5" />,
+    href: "/dashboard",
+    icon: <BarChart3 className="w-6 h-6" />,
   },
 ];
 
