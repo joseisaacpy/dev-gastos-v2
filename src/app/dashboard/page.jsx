f4fa4186f5a82eadef2a8cb7d179a3253241ddbd
José Isaac <joseisaacnascimento@gmail.com>
2025-10-17 22:00:35 -0300

    feat: criar tela de dash placeholder com cards, icons e gráfico

--- src/app/dashboard/page.jsx
+++ src/app/dashboard/page.jsx
@@ -1,7 +1,31 @@
+// Importa o card reaproveitável
+import CardDashboard from "@/components/CardDashboard";
+import ChartOverview from "@/components/ChartOverview";
+// Icons
+import { Users, DollarSign, BarChart2, AlertCircle } from "lucide-react";
+
 export default function Dashboard() {
   return (
-    <section>
-      <h1>Dashboard</h1>
+    <section className="py-4 px-2 flex flex-col gap-4">
+      <h1 className="text-2xl md:text-3xl font-bold">Dashboard</h1>
+      {/* Grid de cards */}
+      <div className="grid grid-cols-2 md:grid-cols-4 gap-4">
+        <CardDashboard titulo="Total de clientes" valor="10" icon={<Users />} />
+        <CardDashboard
+          titulo="Gastos"
+          valor="25"
+          icon={<DollarSign className="text-red-800" />}
+        />
+        <CardDashboard
+          titulo="Lucro"
+          valor="R$ 1.250"
+          icon={<DollarSign className="text-blue-800" />}
+        />
+        <CardDashboard titulo="Novos leads" valor="8" icon={<BarChart2 />} />
+        <CardDashboard titulo="Pendências" valor="3" icon={<AlertCircle />} />
+      </div>
+      {/* Gráficos */}
+      <ChartOverview />
     </section>
   );
 }
